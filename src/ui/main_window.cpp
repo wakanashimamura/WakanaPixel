f5ecb78d885b162cf058cc7495ceafe69cefeb4f
Wakana Shimamura <158538120+wakanashimamura@users.noreply.github.com>
2026-08-04 20:30:44 +0200

    refactor: rewrite project from scratch

---
+++ src/ui/main_window.cpp
@@ -0,0 +1,13 @@
+#include "main_window.h"
+
+#include "ui_main_window.h"
+
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent),
+      m_ui(new Ui::MainWindow) {
+  m_ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+  delete m_ui;
+}
